c9afbc24be97d0fa23c12fd417c5bd78f3172701
Gordon <gordonchi11@gmail.com>
2018-09-22 16:57:05 -0400

    Create Network.py

---
+++ Network.py
@@ -0,0 +1,44 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import math
+
+"""
+jokes on you all this is a python network that doesn't have any significance
+"""
+
+class Network(nn.Module):
+    def __init__(self, num_classes):
+        self.numClasses = num_classes
+        super(ChessConvNet, self).__init__()
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(1, 32, kernel_size=5, stride=1, padding=2),  # 1, 32
+            nn.BatchNorm2d(32),
+            nn.ReLU())
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(32, 32, kernel_size=5, stride=1, padding=2),  # 32, 32
+            nn.BatchNorm2d(32),  # 32
+            nn.ReLU())
+        self.layer3 = nn.Sequential(
+            nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1),  # 32, 32
+            nn.BatchNorm2d(32),
+            nn.ReLU())
+        self.layer4 = nn.Sequential(
+            nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1),  # 32, 32
+            nn.BatchNorm2d(32),
+            nn.ReLU())
+        self.layer5 = nn.Sequential(
+            nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1),  # 32, 32
+            nn.BatchNorm2d(32),
+            nn.ReLU())
+        self.fc = nn.Linear(896 * 32, num_classes)
+
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = self.layer5(out)
+        out = out.reshape(out.size(0), -1)
+        out = self.fc(out)
+        return out
